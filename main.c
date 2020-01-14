25d1e7b7f39636c2a7d55ce962583b7cc81147d7
renan <renan.gdsilva@gmail.com>
2020-01-13 23:39:33 -0300

    Feature: Recorde e algumas refatorações

--- main.c
+++ main.c
@@ -295,12 +295,14 @@ int menu_start(){
     SDL_Surface* startGame = TTF_RenderText_Solid(kono_font, "Explosion!", color_main);
     SDL_Surface* instructions = TTF_RenderText_Solid(kono_font, "Intrucoes", color_main);
     SDL_Surface* exitGame = TTF_RenderText_Solid(kono_font, "Sair", color_main);
+    SDL_Surface* records = TTF_RenderText_Solid(kono_font, "Record", color_main);
 
-    if(!startGame || !instructions || !exitGame ) exit(1);
+    if(!startGame || !instructions || !exitGame || !records ) exit(1);
 
     ins_object(78, 286, startGame, screen, null);
-    ins_object(78, 319, instructions, screen, null);
-    ins_object(78, 355, exitGame, screen, null);
+    ins_object(78, 316, instructions, screen, null);
+    ins_object(78, 346, records, screen, null);
+    ins_object(78, 376, exitGame, screen, null);
 
     while(quit == true){
         start();
@@ -322,13 +324,13 @@ int menu_start(){
         if(keystates[SDLK_UP]){
             Mix_PlayChannel( -1, sound_menu, 0 );
             if(option == 0){
-                option = 2;
+                option = 3;
             } else{
                 option--;
             }
         } else if (keystates[SDLK_DOWN]){
             Mix_PlayChannel( -1, sound_menu, 0 );
-            if(option == 2){
+            if(option == 3){
                 option = 0;
             } else{
                 option++;
@@ -341,25 +343,35 @@ int menu_start(){
             case 0:
                 startGame = TTF_RenderText_Solid(kono_font, "Explosion!", color_menu1);
                 instructions = TTF_RenderText_Solid(kono_font, "Intrucoes", menu_unselected);
+                records = TTF_RenderText_Solid(kono_font, "Record", menu_unselected);
                 exitGame = TTF_RenderText_Solid(kono_font, "Sair", menu_unselected);
                 break;
             case 1:
                 startGame = TTF_RenderText_Solid(kono_font, "Explosion!", menu_unselected);
                 instructions = TTF_RenderText_Solid(kono_font, "Intrucoes", color_menu2);
+                records = TTF_RenderText_Solid(kono_font, "Record", menu_unselected);
                 exitGame = TTF_RenderText_Solid(kono_font, "Sair", menu_unselected);    
                 break;
             case 2:
                 startGame = TTF_RenderText_Solid(kono_font, "Explosion!", menu_unselected);
                 instructions = TTF_RenderText_Solid(kono_font, "Intrucoes", menu_unselected);
-                exitGame = TTF_RenderText_Solid(kono_font, "Sair", color_menu3);
+                records = TTF_RenderText_Solid(kono_font, "Record", color_menu3);
+                exitGame = TTF_RenderText_Solid(kono_font, "Sair", menu_unselected);
+                break;
+            case 3:
+                startGame = TTF_RenderText_Solid(kono_font, "Explosion!", menu_unselected);
+                instructions = TTF_RenderText_Solid(kono_font, "Intrucoes", menu_unselected);
+                records = TTF_RenderText_Solid(kono_font, "Record", menu_unselected);
+                exitGame = TTF_RenderText_Solid(kono_font, "Sair", color_menu1);
                 break;
         }
 
         ins_object(0, 0, telaIntro, screen, null);
 
         ins_object(78, 286, startGame, screen, null);
-        ins_object(78, 319, instructions, screen, null);
-        ins_object(78, 355, exitGame, screen, null);
+        ins_object(78, 316, instructions, screen, null);
+        ins_object(78, 346, records, screen, null);
+        ins_object(78, 376, exitGame, screen, null);
 
         if( SDL_Flip( screen ) == -1 )
         {
@@ -376,6 +388,7 @@ int menu_start(){
     SDL_free(startGame);
     SDL_free(instructions);
     SDL_free(exitGame);
+    SDL_free(records);
     SDL_free(telaIntro);
 
     /*
@@ -386,12 +399,105 @@ int menu_start(){
    return option;
 }
 
-void sort_records(){
-    
+void sort_records(int gamePoint){
+    FILE* fl;
+    char point[20];
+    int points[5];
+    int index = -1;
+
+    if((fl = fopen("Arquivos/records.txt", "r")) == NULL){
+        perror("Erro: fopen");
+        exit(1);
+    }
+
+    while((fscanf(fl, "%s\n", point)) != EOF){
+        points[++index] = atoi(point);
+    }
+
+    fclose(fl);
+
+    if(gamePoint > points[4]){
+        points[4] = gamePoint;
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5-1; j++){
+                if(points[j] < points[j+1]){
+                    int swap = points[j];
+                    points[j] = points[j+1];
+                    points[j+1] = swap;
+                }
+            }
+        }
+    }
+
+    if((fl = fopen("Arquivos/records.txt", "w")) == NULL){
+        perror("Erro: fopen");
+        exit(1);
+    }
+
+    for(int i = 0; i < 5; i++){
+        sprintf(point, "%d", points[i]);
+        fprintf(fl, "%s\n", point);
+    }
+
+    fclose(fl);
 }
 
 void menu_records(){
+    SDL_Surface* telaRecordes = IMG_Load("Intro/megumin_recordes.png");
 
+    if(!telaRecordes) exit(1);
+
+    int quit = true;
+
+    ins_object(0, 0, telaRecordes, screen, null);
+
+    FILE* fl;
+    char point[20];
+    int points[5];
+    int index = -1;
+
+    if((fl = fopen("Arquivos/records.txt", "r")) == NULL){
+        perror("Erro: fopen");
+        exit(1);
+    }
+
+    while((fscanf(fl, "%s\n", point)) != EOF){
+        points[++index] = atoi(point);
+    }
+
+    SDL_Surface* imgPoint; 
+
+    while(quit == true){
+        start();
+
+        while(SDL_PollEvent(&event)){
+            if(event.type == SDL_KEYDOWN){
+                switch(event.key.keysym.sym){
+                    case SDLK_SPACE: quit = false;
+                }
+            }
+        }
+
+        ins_object(0, 0, telaRecordes, screen, null);
+
+        for(int i = 0; i < 5; i++){
+            sprintf(point, "%d", points[i]);
+            imgPoint = TTF_RenderText_Solid(kono_font, point, menu_unselected);
+            ins_object(40, 144 + (i*30), imgPoint, screen, null);
+        }
+
+        if( SDL_Flip( screen ) == -1 )
+        {
+            return 1;
+        }
+
+        if(get_ticks() < 1000 / 8){
+            SDL_Delay( ( 1000 / 8 ) - get_ticks() );
+        }
+    }
+
+    SDL_free(telaRecordes);
 }
 
 void game_over(){
@@ -404,6 +510,8 @@ void game_over(){
     ins_object(0, 0, img, screen, null);
 
     if(SDL_Flip(screen) == -1) exit(1);
+
+    sort_records(HERO.points);
 
     SDL_Delay(5000);
 
@@ -501,20 +609,15 @@ void bomb_timer(){
 void move_actor(){
     HERO.x += HERO.xVel;
 
-    //If the dot went too far to the left or right
     if( ( HERO.x < 0 ) || ( HERO.x + 32 > SCREEN_WIDTH ) )
     {
-        //move back
         HERO.x -= HERO.xVel;
     }
 
-    //Move the dot up or down
     HERO.y += HERO.yVel;
 
-    //If the dot went too far up or down
     if( ( HERO.y < 0 ) || ( HERO.y + 32 > SCREEN_HEIGHT ) )
     {
-        //move back
         HERO.y -= HERO.yVel;
     }
 }
@@ -555,6 +658,7 @@ void game_start(){
 
     HERO.life = 3;
     HERO.points = 0;
+    HERO.time = 300;
 
     while(quit){
         start();
@@ -568,7 +672,7 @@ void game_start(){
             event_keyboard_handle();
 
             if(event.type == SDL_QUIT){
-                quit = false;
+                exit(1);
             }
         }
 
@@ -630,6 +734,8 @@ int main(){
         } else if (option == 1){
             instrucoes();
         } else if(option == 2){
+            menu_records();
+        } else if(option == 3){
             quit = false;
         }
     }
